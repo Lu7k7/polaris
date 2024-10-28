725aefc4127cf13ba857bf344a84483cb146dc9d
Alessandro Nori <alessandro.nori@datadoghq.com>
2024-10-28 10:13:47 +0100

    address review comments + reformat

--- polaris-service/src/main/java/org/apache/polaris/service/PolarisApplication.java
+++ polaris-service/src/main/java/org/apache/polaris/service/PolarisApplication.java
@@ -208,12 +208,15 @@ public class PolarisApplication extends Application<PolarisApplicationConfig> {
       csAware.setConfigurationStore(configurationStore);
     }
 
-    Boolean skipCredentialSubscopingIndirection = configurationStore.getConfiguration(null, PolarisConfiguration.SKIP_CREDENTIAL_SUBSCOPING_INDIRECTION.key);
+    Boolean skipCredentialSubscopingIndirection =
+        configurationStore.getConfiguration(
+            null, PolarisConfiguration.SKIP_CREDENTIAL_SUBSCOPING_INDIRECTION.key);
     TaskHandlerConfiguration taskConfig = configuration.getTaskHandler();
     TaskExecutorImpl taskExecutor =
         new TaskExecutorImpl(taskConfig.executorService(), metaStoreManagerFactory);
     TaskFileIOSupplier fileIOSupplier =
-        new TaskFileIOSupplier(metaStoreManagerFactory, fileIOFactory, skipCredentialSubscopingIndirection);
+        new TaskFileIOSupplier(
+            metaStoreManagerFactory, fileIOFactory, skipCredentialSubscopingIndirection);
     taskExecutor.addTaskHandler(
         new TableCleanupTaskHandler(taskExecutor, metaStoreManagerFactory, fileIOSupplier));
     taskExecutor.addTaskHandler(
